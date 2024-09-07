7bd9ef5ad35bf80360c1de786cc5244291144e02
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-07 22:18:11 +0300

    @ResponseBody poistettu

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -2,12 +2,8 @@ package hh.sof03.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-
-// REMOVE THIS LATER, ONLY HERE SO THAT THE GET-method works properly without template
-@ResponseBody
 public class BookController {
 
     // http://localhost:8080/index
