ae610ac24e2743430cebba86f3145a9b4d570f7b
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-07 22:02:48 +0300

    @ResponseBody lisätty, jolla estetään virheilmoitukset GET /index:ssä
        - @ResponseBody Poistetaan myöhemmin!

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -2,15 +2,19 @@ package hh.sof03.bookstore.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+
+// REMOVE THIS LATER, ONLY HERE SO THAT THE GET-method works properly without template
+@ResponseBody
 public class BookController {
 
     // http://localhost:8080/index
 
     // handle GET-request for /index
     @GetMapping("/index")
-    public String index(){
+    public String index() {
         return "index";
     }
 
