caea8d3622271ad0063bb1a3234f4ae5f564ad50
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-07 21:57:07 +0300

    bookstore -projekti luotu

---
+++ src/main/java/hh/sof03/bookstore/domain/Book.java
@@ -0,0 +1,73 @@
+package hh.sof03.bookstore.domain;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private Integer publicationYear;
+    private String isbn;
+    private Double price;
+
+    public Book(String title, String author, Integer publicationYear, String isbn, Double price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public Book() {
+        this.title = null;
+        this.author = null;
+        this.publicationYear = null;
+        this.isbn = null;
+        this.price = null;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear + ", isbn=" + isbn
+                + ", price=" + price + "]";
+    }
+
+}
---
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -0,0 +1,17 @@
+package hh.sof03.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+    // http://localhost:8080/index
+
+    // handle GET-request for /index
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+}
