c57fc3bf97055b8a2e077206aabec411a77f2637
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-12 17:17:39 +0300

    kirjojen nimet muutettu oikeiksi

--- src/main/java/hh/sof03/bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -21,10 +21,10 @@ public class BookstoreApplication {
 			Book b1 = new Book("Harry Potter ja viisasten kivi", "J.K.Rowling", 1997, "978-0747532699", 20.00);
 			Book b2 = new Book("Harry Potter ja salaisuuksien kammio", "J.K.Rowling", 1998, "978-0747538493", 20.00);
 			Book b3 = new Book("Harry Potter ja Azkabanin vanki", "J.K.Rowling", 1999, "978-0747542155", 20.00);
-			Book b4 = new Book("Harry Potter ja viisasten kivi", "J.K.Rowling", 2000, "978-0747546245", 25.00);
-			Book b5 = new Book("Harry Potter ja viisasten kivi", "J.K.Rowling", 2003, "978-0747551003", 25.00);
-			Book b6 = new Book("Harry Potter ja viisasten kivi", "J.K.Rowling", 2005, "978-0747581086", 25.00);
-			Book b7 = new Book("Harry Potter ja viisasten kivi", "J.K.Rowling", 2007, "978-0545139700", 30.00);
+			Book b4 = new Book("Harry Potter ja liekehtivä pikari", "J.K.Rowling", 2000, "978-0747546245", 25.00);
+			Book b5 = new Book("Harry Potter ja Feeniksin kilta", "J.K.Rowling", 2003, "978-0747551003", 25.00);
+			Book b6 = new Book("Harry Potter ja puoliverinen prinssi", "J.K.Rowling", 2005, "978-0747581086", 25.00);
+			Book b7 = new Book("Harry Potter ja kuoleman varjelukset", "J.K.Rowling", 2007, "978-0545139700", 30.00);
 		};
 	}
 }
