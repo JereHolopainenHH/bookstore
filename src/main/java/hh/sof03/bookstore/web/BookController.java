247d630a09e3835b9f0ad70666fe4879f6571069
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-12 18:51:13 +0300

    lisätty GET /booklist/add ja POST /booklist/save
    C:/Program Files/Git/booklist/add sisältää lomakkeen, jolla lisätään kirja
    
    C:/Program Files/Git/booklist/save tallentaa kirjan BookRepositoryyn

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.repository.BookRepository;
 
 @Controller
@@ -18,16 +20,26 @@ public class BookController {
         return "index";
     }
 
+    // http://localhost:8080/booklist
     @Autowired
     private BookRepository repository;
 
-    // http://localhost:8080/booklist
     @GetMapping("/booklist")
     public String getBooklist(Model model) {
-
         model.addAttribute("booklist", repository.findAll());
-
         return "booklist";
+    }
+
+    @GetMapping("/booklist/add")
+    public String getBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/booklist/save")
+    public String saveBook(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
     }
 
 }
