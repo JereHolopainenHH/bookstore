37bc9b237da903a9f39e8839640e73e6bed4a593
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-12 18:02:42 +0300

    Poistettu turhat importit

--- src/main/java/hh/sof03/bookstore/web/BookController.java
+++ src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -1,14 +1,10 @@
 package hh.sof03.bookstore.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.repository.BookRepository;
 
 @Controller
