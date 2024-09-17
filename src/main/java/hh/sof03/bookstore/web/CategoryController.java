a620a892fd3a9b92a045ed3f0371283a40ecdf73
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-17 18:11:25 +0300

    injektoitu CategoryRepositorio, luotu GET /categorylist/add ja POST /categorylist/save
    GET /categorylist/add näyttää kategorian lisäyslomakesivun
    
    POST /categorylist/save tallentaa uuden kategorian tietokantaan

--- src/main/java/hh/sof03/bookstore/web/CategoryController.java
+++ src/main/java/hh/sof03/bookstore/web/CategoryController.java
@@ -1,15 +1,39 @@
 package hh.sof03.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.sof03.bookstore.domain.Category;
+import hh.sof03.bookstore.repository.CategoryRepository;
+
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CategoryController {
+
+    @Autowired
+    private CategoryRepository repository;
 
     // http://localhost:8080/categorylist
     @GetMapping("/categorylist")
     public String getCategoryList() {
         return "categorylist";
+    }
+
+    // http://localhost:8080/categorylist/add
+    @GetMapping("/categorylist/add")
+    public String getAddForm(Model model) {
+        model.addAttribute(new Category());
+        return "addcategory";
+    }
+
+    // save new category
+    @PostMapping("/category/save")
+    public String saveCategory(Category category) {
+        repository.save(category);
+        return "redirect:/categorylist";
     }
 
 }
