fa5f9eebb2a89582f9eeb59c027dad7b46f60f83
Jere Holopainen <jere.holopainen@myy.haaga-helia.fi>
2024-09-25 09:47:59 +0300

    Luotu GET /api/booklist/{id}, joka palauttaa kirjan id:n perusteella

--- src/main/java/hh/sof03/bookstore/web/BookRestController.java
+++ src/main/java/hh/sof03/bookstore/web/BookRestController.java
@@ -2,9 +2,10 @@ package hh.sof03.bookstore.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+import java.util.Optional;
 
 import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.repository.BookRepository;
@@ -16,10 +17,16 @@ public class BookRestController {
     @Autowired
     private BookRepository repo;
 
+    // http://localhost:8080/api/booklist
     @GetMapping("/booklist")
-    public List<Book> getBooklistRest(){
-        System.out.println("Getting books");
-        return (List<Book>) repo.findAll();
+    public Iterable<Book> getBooklistRest() {
+        return repo.findAll();
+    }
+
+    // http://localhost:8080/api/booklist/1
+    @GetMapping("/booklist/{id}")
+    public Optional<Book> getBookByIdRest(@PathVariable(value = "id") Long id) {
+        return repo.findById(id);
     }
 
 }
